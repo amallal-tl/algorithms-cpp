e1d488c26b84455a98ced794b39037dba86dc824
amallaltlai <amal.lal@outlook.com>
2023-06-16 09:20:15 +0530

    height of tree

---
+++ height of tree.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        int leftHeight = 1, rightHeight = 1, tall = 0;
+        
+        if(root->left==NULL && root->right == NULL)
+            return 0;
+        
+        if(root->left != NULL)
+           leftHeight += height(root->left);
+       
+        if(root->right != NULL)
+          rightHeight+= height(root->right);
+        
+        tall = (leftHeight>rightHeight)?leftHeight:rightHeight;
+        
+        return tall;
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
