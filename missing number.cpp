0b8ea3c88ce7937b8af1b989a802a838697321a5
amallaltlai <amal.lal@outlook.com>
2023-09-26 20:20:57 +0530

    missing number with O(N) time complexity

--- missing number.cpp
+++ missing number.cpp
@@ -1,20 +1,16 @@
+//O{N} complexity
 class Solution{
   public:
     int missingNumber(vector<int>& array, int n) {
-        //int* arr = new int[n];
         int arr[n] = {0};
         int missing = -1;
-        for(int i = 0; i < n; i++){
-            cout << i+1 << "=" << array[i] << endl;
-            if(array[i] == i+1){
-                arr[array[i]] = 1;
-               // cout << array[i] << " : " << arr[array[i]] << endl;
-            }
+        for(auto itr = array.begin(); itr!=array.end(); itr++){
+            arr[*(itr)-1] = 1;
         }
         
-        for(int i = 0; i < n-1; i++){
-            if((arr[i]-1) == 0)
-                missing = i;
+        for(int i = 0; i < n; i++){
+            if((arr[i]) == 0)
+                missing = i+1;
         }
         return missing;
     }
