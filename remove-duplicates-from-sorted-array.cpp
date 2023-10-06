33bc1efe188e13888da21dc0dfbf5a0a7a624c06
amallaltlai <amal.lal@outlook.com>
2023-10-06 19:47:57 +0530

    remove-duplicates-from-sorted-array

---
+++ remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int index = 0;
+        for(int i = 1; i <= nums.size(); i++){
+            if(nums[index] != nums[i]){
+                index += 1;
+                nums[index] = nums[i];
+            }
+        }
+        return index;
+    }
+};
