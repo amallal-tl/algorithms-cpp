b8e3e5c4f4d1ab381e3506ad53e281cc916a2a5d
amallaltlai <amal.lal@outlook.com>
2023-10-10 19:49:35 +0530

    jump-game

---
+++ jump-game.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/jump-game/submissions/
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reachable = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(reachable < i) return false;
+            reachable = max(reachable, nums[i] + i);
+        }
+         return true;         
+    }
+};
