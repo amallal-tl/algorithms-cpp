67e2e698bf25bf5a1471e18b47573a2992a520fb
amallaltlai <amal.lal@outlook.com>
2023-10-25 19:36:07 +0530

    is-subsequence

--- is-subsequence.cpp
+++ is-subsequence.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        
+        int indS = 0, indT = 0;
+        int sLen = s.length(), tLen = t.length();
+        if(sLen == 0) return true;
+        while(indS < sLen && indT < tLen){
+            if(s[indS] == t[indT]){
+                indS++, indT++;
+            } else if(s[indS] != t[indT]){
+                indT++;
+            }
+        }
+        if(indS == sLen) return true;
+        else return false;
     }
 };
