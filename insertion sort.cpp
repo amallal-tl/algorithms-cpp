ce1f5992558894c1dbdf1ec8b04a1cece506c24d
amallaltlai <amal.lal@outlook.com>
2023-11-01 16:02:11 +0530

    Insertion sort refresh

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -3,20 +3,26 @@
 
 using namespace std;
 
+void print(int arr[], int size){
+	for(int i = 0; i < size; i++){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
 int main(){
-	vector<int>vec = {5, 1, 6, 4, 9, 8};
-	for(int i = 1; i < vec.size(); i++){
-		for(int j = i; j > 0; j--)	{
-			if(vec[j-1] > vec[j]){
-				int temp = vec[j];
-				vec[j] = vec[j-1];
-				vec[j-1] = temp;					
+	int arr[6] = {13, 11, 5, 6, 16, 2};
+	int sizeArr = (sizeof(arr)/sizeof(int));
+	print(arr, sizeArr);
+	for(int i = 0; i < sizeArr; i++){
+		for(int j = i; j > 0; j--){
+			if(arr[j] < arr[j-1]){
+				//cout << "Elt : " << arr[j] << " < " << arr[j-1] << endl;
+				int temp = arr[j] + arr[j-1];
+				arr[j-1] = temp - arr[j-1];
+				arr[j] = temp-arr[j];
 			}
 		}
 	}
-	
-	for(auto itr = vec.begin(); itr != vec.end(); itr++){
-		cout << *(itr) << " ";
-	}
-	cout << endl;
+	print(arr, sizeArr);
 }
