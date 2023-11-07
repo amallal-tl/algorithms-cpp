12037a5b4f75fcfc364c03bc96597ae7f7383b4c
amallaltlai <amal.lal@outlook.com>
2023-11-07 21:46:14 +0530

    Passing array as arguments

--- pointers.cpp
+++ pointers.cpp
@@ -3,6 +3,7 @@
                               Online C++ Compiler.
                Code, Compile, Run and Debug C++ program online.
 Write your code in this editor and press "Run" button to compile and execute it.
+https://www.youtube.com/watch?v=zuegQmMdy8M
 
 *******************************************************************************/
 
@@ -10,9 +11,17 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 using namespace std;
 
+int sumOfElements(int* arr, int size){
+    int sum = 0;
+    for(int i = 0; i < size; i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+
 int main()
 {
-    int a = 10;
+    /*int a = 10;
     int* p = &a;
     cout<<"p = " << p << endl;
     cout<<"a = " << *p << endl;
@@ -25,5 +34,11 @@ int main()
     cout<<"*p+1 = " << *p  << endl;
     *p = *(p)+1;
     cout<<"*p+1 = " << *p << endl;
+    */
+    int arr[4] = {4, 3, 2, 1};
+    //cout << "Arr[0] = " << *arr << endl;
+    //cout << "Arr[1] = " << *(arr+1) << endl;
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout << "Sum = " << sumOfElements(arr, size) << endl;
     return 0;
 }
